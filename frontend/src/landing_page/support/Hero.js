1e443f40f5f9dc819e6deafbcd9811a6ec8bfb95
m1s6ah <misbahkhan201678@gmail.com>
2025-10-04 13:33:31 +0530

    feat: Initial commit for dashboard application

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -2,11 +2,31 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div className='container'>
-        <div className='row p-5 mt-5 mb-5'>
-            <h1 className='fs-2'> To create a ticket, select a relevant topic</h1>
+    <section className='container-fluid' id = "supportHero">
+        <div className='p-5' id="supportWrapper">
+            <h4> Support Portal</h4>
+            <a href="">Track Tickets</a>
         </div>
-    </div>
+        <div className='row p-5 m-3'>
+            <div className='col-6 p-5 '>
+                <h1 className="fs-3">Search for an an answer or browse help topics to create a ticket </h1>
+                <input placeholder ="Eg.how do i activated F&O"/>
+                <br/>
+                <a href="">Track account opening </a>
+                <a href="">Track segment activation </a>
+                <a href="">Intraday margin </a>
+                <a href="">Kite user manual </a>
+            </div>
+            <div className='col-6 p-5'>
+               <h1 className='fs-3'>Featured </h1>
+               <ol>
+                <li><a href="">Current Takeovers and Delisting - January 2024</a></li>
+               <li><a href=""> Latest Intraday leverages -MIS & </a>
+         </li>
+               </ol>
+              </div>
+        </div>
+    </section>
   )
 }
 export default Hero;
